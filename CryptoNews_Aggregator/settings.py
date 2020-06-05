c1fd28351c512c1e6dc355ecfdf88f8cf1e27df9
manda-des32 <64405400+manda-des32@users.noreply.github.com>
2020-06-05 20:49:53 +0530

    Error fix v2

--- CryptoNews_Aggregator/settings.py
+++ CryptoNews_Aggregator/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-import whitenoise.django
+import whitenoise.middleware
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
